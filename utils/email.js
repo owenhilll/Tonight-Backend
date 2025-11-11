1b4e4a94235cc735246413a5a68131f370db2b69
Owen Hill <owenhill10@gmail.com>
2025-11-11 09:57:00 -0500

    Refactor sendRegistrationEmail to use Microsoft Graph API
    Replaced nodemailer-based implementation of sendRegistrationEmail with a Microsoft Graph API POST request. This change aligns email sending with the existing support email function and removes Hotmail transporter and related code.

--- utils/email.js
+++ utils/email.js
@@ -3,38 +3,6 @@ let tenantId = process.env.tenantId; // Get from Azure App Registration
 let clientId = process.env.clientId; // Get from Azure App Registration
 let clientSecret = process.env.clientSecret;
 let oAuthToken = "";
-const transporter = nodemailer.createTransport({
-  service: "hotmail",
-
-  auth: {
-    user: process.env.BUSINESS_REG,
-    pass: process.env.BUSINESS_REG_PW,
-  },
-});
-
-export async function sendRegistrationEmail(mailOptions) {
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    return true;
-  } catch (err) {
-    console.log(err);
-    return false;
-  }
-}
-let msgPayload = {
-  //Ref: https://learn.microsoft.com/en-us/graph/api/resources/message#properties
-  message: {
-    subject: "Test",
-    body: {
-      contentType: "HTML",
-      content: "Test123",
-    },
-    toRecipients: [
-      { emailAddress: { address: "meganb@contoso.onmicrosoft.com" } },
-    ],
-  },
-};
-let userFrom = "support@localeapplive.com";
 import * as msal from "@azure/msal-node";
 
 import fetch, { Headers } from "node-fetch";
@@ -61,6 +29,20 @@ const bearer = `Bearer ${tokenInfo.accessToken}`;
 
 headers.append("Authorization", bearer);
 headers.append("Content-Type", "application/json");
+
+export async function sendRegistrationEmail(mailOptions) {
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ message: mailOptions, saveToSentItems: false }),
+  };
+
+  const res = await fetch(
+    graphEndpoint + "/v1.0/users/BusinessReg@localeapplive.com/sendMail",
+    options
+  );
+  return res.status == 202;
+}
 
 export async function sendSupportEmail(mailOptions) {
   const options = {
